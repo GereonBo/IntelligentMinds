426578988c1c20d36105b2f4c5efcabe5e8071ce
Thomas Hoedl <thomas.hoedl@student.tugraz.at>
2015-04-22 13:30:25 +0200

    script debugged

--- create_tables.sql
+++ create_tables.sql
@@ -16,14 +16,16 @@ CREATE TABLE groups
 
 CREATE TABLE user_groups
 (
-    user_id NUMBER PRIMARY KEY,
-    group_id NUMBER PRIMARY KEY
+    user_id NUMBER,
+    group_id NUMBER,
+    CONSTRAINT pk_user_groups PRIMARY KEY (user_id, group_id)
 );
 
 CREATE TABLE user_contacts
 (
-    user_id NUMBER PRIMARY KEY,
-    contact_id NUMBER PRIMARY KEY
+    user_id NUMBER,
+    contact_id NUMBER,
+    CONSTRAINT pk_user_contacts PRIMARY KEY (user_id, contact_id)
 );
 
 CREATE TABLE messages
@@ -37,22 +39,22 @@ CREATE TABLE messages
 );
 
 ALTER TABLE user_groups
-ADD ( CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id) );
+ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
 
 ALTER TABLE user_groups
-ADD ( CONSTRAINT fk_group_id FOREIGN KEY (group_id) REFERENCES groups(id) );
+ADD CONSTRAINT fk_group_id FOREIGN KEY (group_id) REFERENCES groups(id);
 
 ALTER TABLE user_contacts
-ADD ( CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id) );
+ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
 
 ALTER TABLE user_contacts
-ADD ( CONSTRAINT fk_contact_id FOREIGN KEY (contact_id) REFERENCES users(id) );
+ADD CONSTRAINT fk_contact_id FOREIGN KEY (contact_id) REFERENCES users(id);
 
 ALTER TABLE messages
-ADD ( CONSTRAINT fk_user_sender_id FOREIGN KEY (user_sender_id) REFERENCES users(id) );
+ADD CONSTRAINT fk_user_sender_id FOREIGN KEY (user_sender_id) REFERENCES users(id);
 
 ALTER TABLE messages
-ADD ( CONSTRAINT fk_user_receiver_id FOREIGN KEY (user_receiver_id) REFERENCES users(id) );
+ADD CONSTRAINT fk_user_receiver_id FOREIGN KEY (user_receiver_id) REFERENCES users(id);
 
 ALTER TABLE messages
-ADD ( CONSTRAINT fk_group_receiver_id FOREIGN KEY (group_receiver_id) REFERENCES groups(id) );
+ADD CONSTRAINT fk_group_receiver_id FOREIGN KEY (group_receiver_id) REFERENCES groups(id);
