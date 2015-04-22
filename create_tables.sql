e14c34e961c032f6661a212011e30ffa65050be4
intelligentminds <intelligentminds@localhost.localdomain>
2015-04-22 14:41:50 +0200

    Modified to work with mysql

--- create_tables.sql
+++ create_tables.sql
@@ -1,60 +1,49 @@
 CREATE TABLE users
 (
-    id NUMBER PRIMARY KEY,
-    name VARCHAR,
-    user_name VARCHAR,
-    password VARCHAR,
-    email VARCHAR,
-    profile_text VARCHAR
+id INTEGER PRIMARY KEY,
+name VARCHAR(45),
+user_name VARCHAR(45),
+password CHAR(56),
+email VARCHAR(45),
+profile_text VARCHAR(256)
 );
-
 CREATE TABLE groups
 (
-    id NUMBER PRIMARY KEY,
-    name VARCHAR
+id INTEGER PRIMARY KEY,
+name VARCHAR(45)
 );
-
 CREATE TABLE user_groups
 (
-    user_id NUMBER,
-    group_id NUMBER,
-    CONSTRAINT pk_user_groups PRIMARY KEY (user_id, group_id)
+user_id INTEGER,
+group_id INTEGER,
+CONSTRAINT pk_user_groups PRIMARY KEY (user_id, group_id)
 );
-
 CREATE TABLE user_contacts
 (
-    user_id NUMBER,
-    contact_id NUMBER,
-    CONSTRAINT pk_user_contacts PRIMARY KEY (user_id, contact_id)
+user_id INTEGER,
+contact_id INTEGER,
+CONSTRAINT pk_user_contacts PRIMARY KEY (user_id, contact_id)
 );
-
 CREATE TABLE messages
 (
-    id NUMBER PRIMARY KEY,
-    creaton_date DATE,
-    text VARCHAR,
-    user_sender_id NUMBER,
-    user_receiver_id NUMBER,
-    group_receiver_id NUMBER
+id INTEGER PRIMARY KEY,
+creaton_date DATE,
+text VARCHAR(300),
+user_sender_id INTEGER,
+user_receiver_id INTEGER,
+group_receiver_id INTEGER
 );
-
 ALTER TABLE user_groups
 ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
-
 ALTER TABLE user_groups
 ADD CONSTRAINT fk_group_id FOREIGN KEY (group_id) REFERENCES groups(id);
-
 ALTER TABLE user_contacts
-ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
-
+ADD CONSTRAINT fk_contacts_user_id FOREIGN KEY (user_id) REFERENCES users(id);
 ALTER TABLE user_contacts
-ADD CONSTRAINT fk_contact_id FOREIGN KEY (contact_id) REFERENCES users(id);
-
+ADD CONSTRAINT fk_contacts_contact_id FOREIGN KEY (contact_id) REFERENCES users(id);
 ALTER TABLE messages
-ADD CONSTRAINT fk_user_sender_id FOREIGN KEY (user_sender_id) REFERENCES users(id);
-
+ADD CONSTRAINT fk_messages_user_sender_id FOREIGN KEY (user_sender_id) REFERENCES users(id);
 ALTER TABLE messages
-ADD CONSTRAINT fk_user_receiver_id FOREIGN KEY (user_receiver_id) REFERENCES users(id);
-
+ADD CONSTRAINT fk_messages_user_receiver_id FOREIGN KEY (user_receiver_id) REFERENCES users(id);
 ALTER TABLE messages
-ADD CONSTRAINT fk_group_receiver_id FOREIGN KEY (group_receiver_id) REFERENCES groups(id);
+ADD CONSTRAINT fk_messages_group_receiver_id FOREIGN KEY (group_receiver_id) REFERENCES groups(id);
