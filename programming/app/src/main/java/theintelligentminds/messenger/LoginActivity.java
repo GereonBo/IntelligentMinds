61f229539a0ea648bc6ae4cbec7c25dc262e1f7c
Matthias P <matthias.pfeifer@student.tugraz.at>
2015-04-29 16:37:05 +0200

    [MP TH] rewrite of LoginActivity, register and login button work

--- programming/app/src/main/java/theintelligentminds/messenger/LoginActivity.java
+++ programming/app/src/main/java/theintelligentminds/messenger/LoginActivity.java
@@ -10,14 +10,14 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
+import android.widget.Toast;
+import android.view.View.OnClickListener;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -30,282 +30,83 @@ import java.util.List;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends Activity{
 
-  /**
-   * A dummy authentication store containing known user names and passwords.
-   * TODO: remove after connecting to a real authentication system.
-   */
-  private static final String[] DUMMY_CREDENTIALS = new String[] { "foo@example.com:hello", "bar@example.com:world" };
-  /**
-   * Keep track of the login task to ensure we can cancel it if requested.
-   */
-  private UserLoginTask mAuthTask = null;
+
 
   // UI references.
-  private TextView textViewUserName;
-  private EditText textViewPassword;
-  private View mProgressView;
-  private View mLoginFormView;
+  public TextView textViewUserName;
+  public EditText textViewPassword;
+  public Button registerButton;
+  public Button loginButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.login);
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.login);
+      addListenerOnButton();
 
-    // Set up the login form.
-    textViewUserName = (TextView) findViewById(R.id.textfieldUserName);
-    populateAutoComplete();
+  }
 
-    textViewPassword = (EditText) findViewById(R.id.textfieldPassword);
-    textViewPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-        @Override
-        public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-            if (id == R.id.buttonLogin || id == EditorInfo.IME_NULL) {
-                attemptLogin();
-                return true;
+    public void addListenerOnButton(){
+        registerButton = (Button) findViewById(R.id.buttonRegister);
+        loginButton = (Button) findViewById(R.id.buttonLogin);
+
+        registerButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doRegister();
             }
-            return false;
+        });
+
+        loginButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doLogin();
+
+            }
+        });
+
+
+    }
+
+    public void doLogin(){
+    textViewUserName = (TextView) findViewById(R.id.textfieldUserName);
+    textViewPassword = (EditText) findViewById(R.id.textfieldPassword);
+
+        String user_name = textViewUserName.getText().toString();
+        String password = textViewPassword.getText().toString();
+
+        if ((CheckIfUserNameExists(user_name)) == true)
+        {
+            if ((CheckIfPasswordCorrect(user_name, password)) == true)
+            {
+                Toast.makeText(LoginActivity.this," Congratulations, you are logged in!",
+                                    Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(LoginActivity.this,"OOOps wrong password, please try again!",
+                        Toast.LENGTH_SHORT).show();
+               }
         }
-    });
-
-    Button registerButton = (Button) findViewById(R.id.buttonRegister);
-    Button loginButton = (Button) findViewById(R.id.buttonLogin);
-    registerButton.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            //attemptRegister();
+        else{
+            Toast.makeText(LoginActivity.this, "This user name does not exist!",
+                    Toast.LENGTH_SHORT).show();
         }
-    });
+    }
+    public void doRegister(){
 
-      loginButton.setOnClickListener(new OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              attemptLogin();
-          }
-      });
-
-//    mLoginFormView = findViewById(R.id.login_form);
-//    mProgressView = findViewById(R.id.login_progress);
-  }
-
-  private void populateAutoComplete() {
-    getLoaderManager().initLoader(0, null, this);
-  }
-
-  /**
-   * Attempts to sign in or register the account specified by the login form. If
-   * there are form errors (invalid email, missing fields, etc.), the errors are
-   * presented and no actual login attempt is made.
-   */
-
-  public void attemptLogin() {
-    if (mAuthTask != null) {
-      return;
+        Toast.makeText(LoginActivity.this," You clicked RegisterButton!",
+                Toast.LENGTH_SHORT).show();
     }
 
-
-
-    // Reset errors.
-    textViewUserName.setError(null);
-    textViewPassword.setError(null);
-
-    // Store values at the time of the login attempt.
-    String email = textViewUserName.getText().toString();
-    String password = textViewPassword.getText().toString();
-
-    boolean cancel = false;
-    View focusView = null;
-
-    // Check for a valid password, if the user entered one.
-    if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-      textViewPassword.setError(getString(R.string.error_invalid_password));
-      focusView = textViewPassword;
-      cancel = true;
+    public boolean CheckIfUserNameExists(String userName){
+        return true;
     }
 
-    // Check for a valid email address.
-    if (TextUtils.isEmpty(email)) {
-      textViewUserName.setError(getString(R.string.error_field_required));
-      focusView = textViewUserName;
-      cancel = true;
+    public boolean CheckIfPasswordCorrect(String userName, String password){
+        return true;
     }
-    else if (!isEmailValid(email)) {
-      textViewUserName.setError(getString(R.string.error_invalid_email));
-      focusView = textViewUserName;
-      cancel = true;
-    }
-
-    if (cancel) {
-      // There was an error; don't attempt login and focus the first
-      // form field with an error.
-      focusView.requestFocus();
-    }
-    else {
-      // Show a progress spinner, and kick off a background task to
-      // perform the user login attempt.
-      showProgress(true);
-      mAuthTask = new UserLoginTask(email, password);
-      mAuthTask.execute((Void) null);
-    }
-  }
-
-  private boolean isEmailValid(String email) {
-    // TODO: Replace this with your own logic
-    return email.contains("@");
-  }
-
-  private boolean isPasswordValid(String password) {
-    // TODO: Replace this with your own logic
-    return password.length() > 4;
-  }
-
-  /**
-   * Shows the progress UI and hides the login form.
-   */
-
-  @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-  public void showProgress(final boolean show) {
-    // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-    // for very easy animations. If available, use these APIs to fade-in
-    // the progress spinner.
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-      int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-      mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-      mLoginFormView.animate().setDuration(shortAnimTime).alpha(show ? 0 : 1)
-          .setListener(new AnimatorListenerAdapter() {
-              @Override
-              public void onAnimationEnd(Animator animation) {
-                  mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-              }
-          });
-
-      mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-      mProgressView.animate().setDuration(shortAnimTime).alpha(show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-        @Override
-        public void onAnimationEnd(Animator animation) {
-          mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-        }
-      });
-    }
-    else {
-      // The ViewPropertyAnimator APIs are not available, so simply show
-      // and hide the relevant UI components.
-      mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-      mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-    }
-  }
-
-  @Override
-  public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-    return new CursorLoader(
-        this,
-        // Retrieve data rows for the device user's 'profile' contact.
-        Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI, ContactsContract.Contacts.Data.CONTENT_DIRECTORY),
-        ProfileQuery.PROJECTION,
-
-        // Select only email addresses.
-        ContactsContract.Contacts.Data.MIMETYPE + " = ?",
-        new String[] { ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE },
-
-        // Show primary email addresses first. Note that there won't be
-        // a primary email address if the user hasn't specified one.
-        ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-  }
-
-  @Override
-  public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-    List<String> emails = new ArrayList<String>();
-    cursor.moveToFirst();
-    while (!cursor.isAfterLast()) {
-      emails.add(cursor.getString(ProfileQuery.ADDRESS));
-      cursor.moveToNext();
-    }
-
-    addEmailsToAutoComplete(emails);
-  }
-
-  @Override
-  public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-  }
-
-  private interface ProfileQuery {
-    String[] PROJECTION = { ContactsContract.CommonDataKinds.Email.ADDRESS,
-        ContactsContract.CommonDataKinds.Email.IS_PRIMARY, };
-
-    int ADDRESS = 0;
-    int IS_PRIMARY = 1;
-  }
-
-  private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-    // Create adapter to tell the AutoCompleteTextView what to show in its
-    // dropdown list.
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(LoginActivity.this,
-        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-//    textViewUserName.setAdapter(adapter);
-  }
-
-  /**
-   * Represents an asynchronous login/registration task used to authenticate the
-   * user.
-   */
-
-  public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-    private final String mEmail;
-    private final String mPassword;
-
-    UserLoginTask(String email, String password) {
-      mEmail = email;
-      mPassword = password;
-    }
-
-    @Override
-    protected Boolean doInBackground(Void... params) {
-      // TODO: attempt authentication against a network service.
-
-      try {
-        // Simulate network access.
-        Thread.sleep(2000);
-      }
-      catch (InterruptedException e) {
-        return false;
-      }
-
-      for (String credential : DUMMY_CREDENTIALS) {
-        String[] pieces = credential.split(":");
-        if (pieces[0].equals(mEmail)) {
-          // Account exists, return true if the password matches.
-          return pieces[1].equals(mPassword);
-        }
-      }
-
-      // TODO: register the new account here.
-      return true;
-    }
-
-    @Override
-    protected void onPostExecute(final Boolean success) {
-      mAuthTask = null;
-      showProgress(false);
-
-      if (success) {
-        finish();
-      }
-      else {
-        textViewPassword.setError(getString(R.string.error_incorrect_password));
-        textViewPassword.requestFocus();
-      }
-    }
-
-    @Override
-    protected void onCancelled() {
-      mAuthTask = null;
-      showProgress(false);
-    }
-  }
 
 }
