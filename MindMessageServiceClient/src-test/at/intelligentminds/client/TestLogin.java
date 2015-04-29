6028829580e3d2a0789e79777809333269f52f04
DerManiac <dermaniac@gmail.com>
2015-04-29 12:51:10 +0200

    [TW, RS] Added login validation test

--- MindMessageServiceClient/src-test/at/intelligentminds/client/TestLogin.java
+++ MindMessageServiceClient/src-test/at/intelligentminds/client/TestLogin.java
@@ -49,5 +49,27 @@ public class TestLogin {
     assertNotNull(loginFails);
     assertEquals("", loginFails);
   }
-  
+
+  @Test
+  /**
+   * Tests if login is remembered
+   */
+  public void testLoginRemembered()
+  {
+    String token_user_a = provider.performLogin("test", "p@$$w0rd");
+    String token_user_b = provider.performLogin("demo", "demopassword");
+    
+    assertNotNull(token_user_a);
+    assertNotNull(token_user_b);
+    assertNotEquals(token_user_a, "");
+    assertNotEquals(token_user_b, "");
+    
+    boolean a_still_logged_in = provider.validateLogin(token_user_a);
+    boolean b_still_logged_in = provider.validateLogin(token_user_b);
+    boolean c_still_logged_in = provider.validateLogin("somegarbagetoken");
+    
+    assertTrue(a_still_logged_in);
+    assertTrue(b_still_logged_in);
+    assertFalse(c_still_logged_in);
+  }
 }
