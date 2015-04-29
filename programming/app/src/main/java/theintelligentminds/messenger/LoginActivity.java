2b770210181e4ab5581700c7c580d4ee1e4da0da
Matthias P <matthias.pfeifer@student.tugraz.at>
2015-04-29 09:57:49 +0200

    Commit2

--- programming/app/src/main/java/theintelligentminds/messenger/LoginActivity.java
+++ programming/app/src/main/java/theintelligentminds/messenger/LoginActivity.java
@@ -56,7 +56,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lo);
+        setContentView(R.layout.activity_login);
 
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
