a9f9d8b80342aff38bd40ce37c3e02d038d35365
Thomas Niedermayr <thomas.niedermayr@student.tugraz.at>
2015-05-13 13:56:53 +0200

    [TW,TN] Added User Search Service

--- MindMessageServiceClient/src/at/intelligentminds/client/ConnectionProvider.java
+++ MindMessageServiceClient/src/at/intelligentminds/client/ConnectionProvider.java
@@ -95,4 +95,5 @@ public class ConnectionProvider {
 
     return response;
   }
+  
 }
---
+++ MindMessagesService/src/at/intelligentminds/service/UserService.java
@@ -0,0 +1,52 @@
+package at.intelligentminds.service;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.hibernate.Criteria;
+import org.hibernate.Transaction;
+
+import at.intelligentminds.service.model.HibernateSupport;
+import at.intelligentminds.service.model.User;
+
+@Path("/userservice")
+public class UserService {
+
+  @POST
+  @Path("/searchuser")
+  @Produces(MediaType.TEXT_PLAIN)
+  public List searchUser(@FormParam("searchText") String searchText) {
+    
+    String[] parts = searchText.split(" ");
+
+    Transaction tx = HibernateSupport.getSession().beginTransaction();
+    Criterion cfirstname = Restrictions.like("email", ""+searchText+"%");
+    Criterion clastname = Restrictions.like("firstName", "%"+searchText+"%");
+    Criterion cemail = Restrictions.like("lastName", "%"+searchText+"%");
+    Criterion cemail = Restrictions.like("accountName", "%"+searchText+"%");
+    
+    if(parts.length > 1)
+    {
+          for(int i = 0; i < parts.length;i++)
+          {
+            Criterion cfirstname = Restrictions.like("email", ""+parts[i]+"%");
+            Criterion clastname = Restrictions.like("firstName", "%"+parts[i]+"%");
+            Criterion cemail = Restrictions.like("lastName", "%"+parts[i]+"%");
+            Criterion cemail = Restrictions.like("accountName", "%"+parts[i]+"%");
+          }
+    }
+        
+    Criteria criteria = HibernateSupport.getSession().createCriteria(User.class);
+    criteria.add(Restrictions.or(cfirstname,clastname,cemail));
+    criteria.setProjection(Projection.property("firstName"));
+    criteria.setProjection(Projection.property("lastName"));
+    criteria.setProjection(Projection.property("accountName"));
+    
+    return criteria.list();
+  }
+  
+}
