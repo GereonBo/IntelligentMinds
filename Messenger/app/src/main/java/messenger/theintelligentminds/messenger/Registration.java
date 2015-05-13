3c6fe85de629fba5f6a1655b605eee13d50e6a07
Thomas Hoedl <thomas.hoedl@student.tugraz.at>
2015-05-13 12:52:47 +0200

    [RS, TH] implementing server connection in client

--- Messenger/app/src/main/java/messenger/theintelligentminds/messenger/Registration.java
+++ Messenger/app/src/main/java/messenger/theintelligentminds/messenger/Registration.java
@@ -1,71 +1,77 @@
 package messenger.theintelligentminds.messenger;
 
 import android.app.Activity;
-import android.app.Application;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 
 import android.os.Bundle;
 
 import at.intelligentminds.client.ConnectionProvider;
-import messenger.theintelligentminds.messenger.Profile;
-import android.view.KeyEvent;
+
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import java.lang.reflect.Array;
 
 /**
  * Created by Chris_1909 on 29.04.2015.
  */
 
-public class Registration extends Activity{
-    private Button Register;
-    private ConnectionProvider provider = ConnectionProvider.getInstance();
+public class Registration extends Activity {
+  private Button register;
+  private EditText firstName;
+  private EditText lastName;
+  private EditText email;
+  private EditText password;
+  private ConnectionProvider provider = ConnectionProvider.getInstance();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.registration);
-        Register = (Button) findViewById(R.id.buttonRegister);
-        final EditText FirstName = (EditText) findViewById(R.id.editTextFirstname);
-/*
-        FirstName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-                                                @Override
-                                                public boolean onEditorAction(TextView textView, int i,KeyEvent keyEvent){
-                                                    boolean handled = false;
-                                                    if(i== EditorInfo.IME_ACTION_NEXT){
-                                                        String inputText = textView.getText().toString();
-                                                        Toast.makeText(Registration.this, "Your name is: " +
-                                                        inputText, Toast.LENGTH_SHORT).show();
-                                                    }
-                                                    return handled;
-                                                }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.registration);
 
+    register = (Button) findViewById(R.id.buttonRegister);
+    firstName = (EditText) findViewById(R.id.editTextFirstname);
+    lastName = (EditText) findViewById(R.id.editTextLastname);
+    email = (EditText) findViewById(R.id.textfieldEMail);
+    password = (EditText) findViewById(R.id.textfieldPassword);
 
-                                            });
-*/
-                Register.setOnClickListener(new OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
+    register.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        String message;
 
-                        Intent intent = new Intent(Registration.this, Profile.class);
-                        //intent.putExtra("key", FirstName.getText().toString());
-                        startActivity(intent);
-                    }
-
-                });
+        ConnectionProvider.RegisterResponse response = provider.register(email.getText().toString(),
+                password.getText().toString(), "male", firstName.getText().toString(),
+                lastName.getText().toString());
 
 
 
+        switch(response) {
+            case EMAIL: message = "wrong email address"; break;
+            case ERROR: message = "an error has occurred"; break;
+            default: case MISC_ERROR: message = "an unexpected error has occurred"; break;
+            case NAME: message = "invalid format at name"; break;
+            case PASSWORD: message = "invalid format at name"; break;
+            case SUCCESS: message = "Registration has been successful"; break;
+            case USER_EXISTS: message = "The user exists already"; break;
 
+        }
 
-    }
+        new AlertDialog.Builder(getApplicationContext()).setTitle("Invalid Input").setMessage(message).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
 
+            }
+        }).show();
+      }
+
+    });
+
+  }
 
 }
