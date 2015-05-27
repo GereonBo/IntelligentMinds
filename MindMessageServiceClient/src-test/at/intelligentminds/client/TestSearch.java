9a10532a037bd2d367df0253abc8ebc7eed39715
Thomas Woger <t.woger@student.tugraz.at>
2015-05-27 15:39:11 +0200

    [TW, RS, JM] refactor userservice

--- MindMessageServiceClient/src-test/at/intelligentminds/client/TestSearch.java
+++ MindMessageServiceClient/src-test/at/intelligentminds/client/TestSearch.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import org.json.JSONArray;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestSearch {
